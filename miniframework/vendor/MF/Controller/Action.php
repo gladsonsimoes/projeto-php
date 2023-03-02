69ed88571236ec08ae10191563a89a774a3ff173
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-03-02 20:27:04 -0300

    Imprimir no site Layout1 com o Action

--- miniframework/vendor/MF/Controller/Action.php
+++ miniframework/vendor/MF/Controller/Action.php
@@ -1,19 +1,27 @@
-<?php 
+<?php
 namespace MF\Controller;
 
-abstract class Action{
-    protected $view;
+abstract class Action
+{
+    protected $view = 'teste';
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->view = new \stdClass();
     }
 
     protected function render($view)
     {
+        $this->view->page = $view;
+        require_once("../App/Views/layout1.phtml");
+    }
+
+    protected function content($view)
+    {
         $className = get_class($this); //retorna da classe
         $className = str_replace('App\\Controllers\\', '', $className); //substituir App\\Controllers\\ para nada ('') 
         $className = strtolower(str_replace('Controller', '', $className)); //substituir o controller para nada , e utilizando strtolower para deixar o nome Index para minusculo index
-        require_once '../App/Views/'. $className.'/'. $view . '.phtml';
+        require_once '../App/Views/' . $className . '/' . $view . '.phtml';
     }
 }
 
