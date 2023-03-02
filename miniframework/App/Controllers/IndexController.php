647eafefd20892276c0904eb7de0059980a5bee7
Gladson Simões <gladsonssimoes@outlook.com.br>
2023-03-01 21:40:59 -0300

    Add files via upload

--- miniframework/App/Controllers/IndexController.php
+++ miniframework/App/Controllers/IndexController.php
@@ -4,21 +4,27 @@ namespace App\Controllers;
 
 class IndexController
 {
+    private $view;
+
+    public function __construct(){
+        $this->view = new \stdClass();
+    }
+
     public function index()
     {
-        $dados = array('Sofá', 'Cama', 'Travesseiro'); //simulacao de query do banco de dados
-        $this->render('sobreNos', $dados);
+        $this->view->dados=array('Sofá' , 'Cama' , 'Traveseeiro'); //simulacao de query do banco de dados
+        $this->render('index');
     }
 
     public function sobreNos()
     {
-        $dados = array('Celular', 'Notebook', 'Carro');
-        $this->render('sobreNos', $dados);
+        $this->view->dados=array('hehe' , 'Camaro' , 'Trovao');
+        $this->render('sobreNos');
     }
 
 
     //funcao para selecionar cada view em vez de escrever monte de require_once dentro das funcoes
-    public function render($view, $dados)
+    public function render($view)
     {
         $className = get_class($this); //retorna da classe
         $className = str_replace('App\\Controllers\\', '', $className); //substituir App\\Controllers\\ para nada ('') 
