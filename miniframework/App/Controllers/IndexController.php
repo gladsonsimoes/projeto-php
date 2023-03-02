8b59b71d106e178ef04066e661c9bd92a09574ce
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-03-02 19:15:00 -0300

    Removi os rquisitos funcionais do IndexController

--- miniframework/App/Controllers/IndexController.php
+++ miniframework/App/Controllers/IndexController.php
@@ -14,19 +14,7 @@ class IndexController
     {
         $this->view->dados=array('Celular' , 'Computador' , 'Monitor');
         $this->render('sobreNos');
-    }
-
-
-    //funcao para selecionar cada view em vez de escrever monte de require_once dentro das funcoes
-    public function render($view)
-    {
-        $className = get_class($this); //retorna da classe
-        $className = str_replace('App\\Controllers\\', '', $className); //substituir App\\Controllers\\ para nada ('') 
-        $className = strtolower(str_replace('Controller', '', $className)); //substituir o controller para nada , e utilizando strtolower para deixar o nome Index para minusculo index
-        require_once '../App/Views/'. $className.'/'. $view . '.phtml';
-    }
-
-  
+    }  
 }
 
 ?>
