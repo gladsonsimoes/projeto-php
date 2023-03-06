492f93f4932b38e0d85cd1b06264af94157f4bf0
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-03-06 20:27:41 -0300

    Chamando query dentro de db

--- miniframework/App/Models/Produto.php
+++ miniframework/App/Models/Produto.php
@@ -1,12 +1,16 @@
-<?php 
-namespace App\Models;
+<?php
+    namespace App\Models;
 
-class Produto{
-    protected $db;
+    class Produto{
+        protected $db;
 
-    public function __construct(PDO $db){
-        $this->db = $db;
+        public function __construct(\PDO $db){
+            $this->db = $db;
+        }
+        public function getProdutos(){
+            // return array('Caderno','Mesa','Cadeira');
+            $query = 'SELECT id, descricao, preco from tb_produtos';
+            $this->db->$query;
+        }
     }
-}
-
 ?>
